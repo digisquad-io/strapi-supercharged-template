64762274675f0bb0bfd541d1f5278c0a28d09219
Sacha STAFYNIAK <sacha.stafyniak@gmail.com>
2021-08-14 00:54:48 +0200

    chore(typing): simplify entities and add missing fields utilities

--- types/entities.d.ts
+++ types/entities.d.ts
@@ -5,16 +5,32 @@ export interface Entity {
 }
 export interface CollectionEntity extends Entity {}
 export interface SingleTypeEntity extends Entity {}
-export interface Component extends Entity {}
-export type RelationWith<T extends Entity> = T | number | null;
+export interface Component extends Partial<Entity> {}
+export type RelationWith<T extends Partial<Entity>> =
+  | T
+  | number
+  | null;
 
-export type ComponentField<T extends Component> = RelationWith<T>;
+export type ComponentField<T extends Component> = Partial<
+  RelationWith<T>
+> &
+  Record<string, any>;
 export type DateTimeField = string;
+export type TimeField = string;
+export type UIDField = string;
 export type JSONField = any;
 
 export type TimestampFields = {
   created_at: DateTimeField;
   updated_at: DateTimeField;
+};
+export type DraftAndPublishFields = {
+  published_at: DateTimeField | null;
+};
+
+export type CreatorsFields = {
+  created_by: RelationWith<UserEntity>;
+  updated_by: RelationWith<UserEntity>;
 };
 
 // @todo: strapi-plugin-user-permissions
